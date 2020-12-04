b8885d6efeb8a8bcbf644cc7e1244648d2cc0d18
python1 <alextomilovski@gmail.com>
2020-12-04 19:13:41 +0300

    04.12

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+from Cards import *
+from Converter import *
+from PlayerCards import *
+
+
+def main():
+    i, j = divide(input2())
+    x, y = to_player_cards(i, j)
+    i, j = to_str(x, y)
+    z = PlayerCards(x, y)
+    f = z.pairs()
+    print(f)
+
+
 def input2():
     res = str(input("input cards  "))
     return res
 
+main()
 
