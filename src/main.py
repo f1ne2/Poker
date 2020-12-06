34cd1b6e8e4f95c21be76157f59cbd901c88a6c3
python1 <alextomilovski@gmail.com>
2020-12-06 19:09:14 +0300

    06.12

--- src/main.py
+++ src/main.py
@@ -8,8 +8,22 @@ def main():
     x, y = to_player_cards(i, j)
     i, j = to_str(x, y)
     z = PlayerCards(x, y)
-    f = z.pairs()
-    print(f)
+    z2 = z.redefine()
+    for c in range(len(z2)):
+        res = z.is_high_card(z2[c])
+        res = z.is_pairs(z2[c])
+        res = z.is_two_pairs(z2[c])
+        res = z.is_set(z2[c])
+        res = z.is_four_of_kind(z2[c])
+        res = z.is_full_house(z2[c])
+        res = z.is_straight(z2[c])
+        res = z.is_flush(z2[c])
+        res2 = z.is_straight_flush(z2[c], res)
+        res = z.is_straight(z2[c])
+        print(res)
+
+
+
 
 
 def input2():
