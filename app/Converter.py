327fc00d4ff84dd640597012f412210f45044df9
python1 <alextomilovski@gmail.com>
2020-12-11 21:46:04 +0300

    11.12

--- app/Converter.py
+++ app/Converter.py
@@ -1,80 +1,64 @@
-from typing import List
-from Cards import Suit, Rank, Card
+from typing import List, Dict
+
+from app.Cards import Suit, Rank, Card
+from app.PlayerCards import PlayerCards
 
 
-def to_player_cards(input_cards: str) -> List[Card] and List[Card]:
+def to_cards(cards_str: str) -> List[Card]:
     j = 0
-    common = []
-    custom = []
-    for i in range(len(input_cards)-1):
+    output = []
+    for i in range(len(cards_str) - 1):
         if i == j:
-            if input_cards[i] != " ":
-                if input_cards[i] != "1":
-                    card1 = Card(set_rank.get(input_cards[i]),
-                                 set_suit.get(input_cards[i+1]))
-                    if len(common) < 5:
-                        common.append(card1)
-                        j += 2
-                    else:
-                        custom.append(card1)
-                        j += 2
-                else:
-                    card1 = Card(set_rank.get(input_cards[i]+input_cards[i+1]),
-                                 set_suit.get(input_cards[i+2]))
-                    if len(common) < 5:
-                        common.append(card1)
-                        j += 3
-                    else:
-                        custom.append(card1)
-                        j += 3
+            if cards_str[i] != "1":
+                output.append(Card(ranks_dict.get(cards_str[i]),
+                                   suits_dict.get(cards_str[i + 1])))
+                j += 2
             else:
-                j += 1
+                output.append(Card(ranks_dict.get(cards_str[i] +
+                                                  cards_str[i + 1]),
+                                   suits_dict.get(cards_str[i + 2])))
+                j += 3
+    for k in range(len(output)):
+        print(output[k].rank, output[k].suit)
+    return output
 
-    return common, custom
+
+def to_player_cards(input_string: str) -> List[PlayerCards]:
+    string_cards = input_string.split(" ")
+    common_cards = to_cards(string_cards[0])
+    return [PlayerCards(common_cards, to_cards(string_card)) for string_card in
+            string_cards[1:]]
 
 
-def to_str(common_to_str: List[Card], custom_to_str: List[Card]) -> str:
-    common_res = ""
-    hands_res = ""
+def to_str(cards_to_str: List[PlayerCards]) -> str:
     res = ""
-
-    for i in range(5):
-        for key, value in set_rank.items():
-            if common_to_str[i].rank == value:
-                common_res = common_res + key
-        for key, value in set_suit.items():
-            if common_to_str[i].suit == value:
-                common_res = common_res + key
-    for i in range(len(custom_to_str)):
-        for key, value in set_rank.items():
-            if custom_to_str[i].rank == value:
-                res = res + key + " "
-        for key, value in set_suit.items():
-            if custom_to_str[i].suit == value:
-                res = res + key + " "
-    k = 0
-    j = 0
-    print(res)
-    for i in range(len(res)-1):
-        if i == j:
-            if res[i] != " ":
-                hands_res = hands_res + res[i]
-                j += 1
-            else:
-                k += 1
-                j += 1
-                if k % 4 == 0:
-                    hands_res = hands_res + " "
-    res = common_res + " " + hands_res
+    for i in range(len(cards_to_str[0].common_cards)):
+        for key, value in ranks_dict.items():
+            if cards_to_str[i].common_cards[i].rank == value:
+                res = res + key
+        for key, value in suits_dict.items():
+            if cards_to_str[i].common_cards[i].suit == value:
+                res = res + key
+    res = res + " "
+    for i in range(len(cards_to_str)):
+        for j in range(len(cards_to_str[0].custom)):
+            for key, value in ranks_dict.items():
+                if cards_to_str[i].custom[j].rank == value:
+                    res = res + key
+            for key, value in suits_dict.items():
+                if cards_to_str[i].custom[j].suit == value:
+                    res = res + key
+        res = res + " "
+    res = res.rstrip(" ")
     return res
 
 
-set_rank = {"2": Rank.Two, "3": Rank.Three, "4": Rank.Four, "5": Rank.Five,
-            "6": Rank.Six, "7": Rank.Seven, "8": Rank.Eight, "9": Rank.Nine,
-            "10": Rank.Ten, "J": Rank.J, "Q": Rank.Q, "K": Rank.K, "A": Rank.A}
-set_suit = {"h": Suit.hearts, "d": Suit.diamonds, "c": Suit.clubs,
-            "s": Suit.spades}
-
-
+ranks_dict: Dict[str, Rank] = {"2": Rank.Two, "3": Rank.Three, "4": Rank.Four,
+                               "5": Rank.Five, "6": Rank.Six, "7": Rank.Seven,
+                               "8": Rank.Eight, "9": Rank.Nine, "10": Rank.Ten,
+                               "J": Rank.J, "Q": Rank.Q, "K": Rank.K,
+                               "A": Rank.A}
+suits_dict: Dict[str, Suit] = {"h": Suit.hearts, "d": Suit.diamonds,
+                               "c": Suit.clubs, "s": Suit.spades}
 
 
